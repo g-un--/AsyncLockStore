7a60e8198bb46653c12b8968214f7cc8c2199dba
Gino Ungureanu <gino.ungureanu@uipath.com>
2017-12-13 16:08:40 +0200

    AsyncLockStore

---
+++ AsyncLockStore.Tests/AsyncLockStoreTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+using System.Linq;
+
+namespace AsyncLockStore.Tests
+{
+    public class AsyncLockStoreTests
+    {
+        [Fact]
+        public async Task ShouldWaitForSameKey()
+        {
+            //Arrange
+            var store = new LockStore();
+            var tasks = new List<Task>();
+            var log = new List<int>();
+            var tasksCount = 16;
+            var childTasksReached = Enumerable.Range(0, tasksCount).Select(_ => new TaskCompletionSource<bool>()).ToList();
+
+            //Act
+            for (var i = 0; i < tasksCount; i++)
+            {
+                var id = i;
+                tasks.Add(Task.Run(async () =>
+                {
+                    childTasksReached[id].SetResult(true);
+                    await Task.WhenAll(childTasksReached.Select(x => x.Task));
+                    using (await store.GetTokenAsync("test"))
+                    {
+                        lock (log) { log.Add(id); };
+                        await Task.Delay(10);
+                        lock (log) { log.Add(id); };
+                    }
+                }));
+            }
+            await Task.WhenAll(tasks);
+
+            //Assert
+            for (var i = 0; i < tasksCount; i++)
+            {
+                log[2 * i].ShouldBe(log[2 * i + 1]);
+            }
+        }
+
+        [Fact]
+        public async Task ShouldNotWaitForDifferentKey()
+        {
+            //Arrange
+            var store = new LockStore();
+            var tasks = new List<Task>();
+            var log = new List<int>();
+            var tasksCount = 16;
+            var childTasks = Enumerable.Range(0, tasksCount).Select(_ => new TaskCompletionSource<bool>()).ToList(); 
+
+            //Act
+            for (var i = 0; i < tasksCount; i++)
+            {
+                var id = i;
+                tasks.Add(Task.Run(async () =>
+                {
+                    using (await store.GetTokenAsync("test" + id))
+                    {
+                        lock (log) { log.Add(id); };
+                        childTasks[id].SetResult(true);
+                        await Task.WhenAll(childTasks.Select(x => x.Task));
+                        lock (log) { log.Add(id); };
+                    }
+                }));
+            }
+            await Task.WhenAll(tasks);
+
+            //Assert
+            log.Take(tasksCount).Distinct().Count().ShouldBe(tasksCount);
+            log.Skip(tasksCount).Distinct().Count().ShouldBe(tasksCount);
+        }
+    }
+}
---
+++ AsyncLockStore/LockStore.fs
@@ -0,0 +1,37 @@
+﻿namespace AsyncLockStore
+
+open System
+open System.Collections.Generic
+
+type internal StoreMessage = GetToken of string * AsyncReplyChannel<IDisposable> | Continue of string
+
+type public LockStore() = 
+
+    let lockStore = 
+        MailboxProcessor<StoreMessage>.Start(fun mbox ->
+            let rec handle (lockedKeys : Dictionary<string, List<AsyncReplyChannel<IDisposable>>>) = 
+                async {
+                    let! message = mbox.Receive()
+                
+                    match message with
+                    | GetToken(key, replyChannel) -> 
+                        if lockedKeys.ContainsKey(key) then
+                            lockedKeys.[key].Add(replyChannel)
+                        else
+                            lockedKeys.Add(key, new List<AsyncReplyChannel<IDisposable>>())
+                            replyChannel.Reply({ new IDisposable with member x.Dispose() = mbox.Post(Continue(key)) })
+                    | Continue(key) -> 
+                        let pending = lockedKeys.[key]
+                        if pending.Count > 0 then
+                            let next = pending.[0]
+                            pending.RemoveAt(0)
+                            next.Reply({ new IDisposable with member x.Dispose() = mbox.Post(Continue(key)) }) 
+                        else
+                            lockedKeys.Remove(key) |> ignore
+                
+                    return! handle lockedKeys
+                }
+            handle (new Dictionary<string, List<AsyncReplyChannel<IDisposable>>>()))
+
+    member this.GetTokenAsync key = 
+        lockStore.PostAndAsyncReply(fun replyChannel -> GetToken(key, replyChannel)) |> Async.StartAsTask
